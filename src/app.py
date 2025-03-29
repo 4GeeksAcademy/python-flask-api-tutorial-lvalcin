033214a937daf443994974bc5f529d89a9a6988f
Lovnyth Valcin <lovnyth@gmail.com>
2025-03-29 02:48:55 +0000

    success api delete method using python

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask import Flask, request
 app = Flask(__name__)
 
 todos = [
@@ -11,6 +12,21 @@ def hello_world():
     json_text = jsonify(todos)
     return json_text
 
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    updated_list = todos.pop(position)
+    return jsonify(todos)
+
+    my_list = [1, 2, 3, 2]
+    my_list.remove(2)
+    print(my_list)  # Output: [1, 3, 2]
 
 
 
